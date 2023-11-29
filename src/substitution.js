d93ea732dec6d251385bd24af5142c03c9fd497a
Lucinda Peal <lucindap2002@gmail.com>
2023-11-28 20:32:45 -0500

    added code for encoding/decoding + error checks

--- src/substitution.js
+++ src/substitution.js
@@ -5,25 +5,44 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-
+ 
   function translate(input, from, to) {
     const result = [];
     const message = input.toLowerCase().split("")
 
     //! look out for if currentIndex = -1 in terms of errors
-    message.forEach((letter) => {
-      
+    message.forEach((letter) => { 
+       const originalIndex = from.findIndex((fromLetter) => fromLetter===letter);
+       if(originalIndex === -1) {
+        result.push(letter);
+       }
+       const translatedLetter = to[originalIndex];
+       result.push(translatedLetter);
     });
+    return result;
   };
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    if(!alphabet||alphabet.length!==26) {
+      return false;
+    };
+
     const  originalAlphabet= "abcdefghijklmnopqrstuvwxyz".split("");
     const encodedAlphabet = alphabet.split("");
 
+    const letterSet = {};
+    encodedAlphabet.forEach((letter) => {
+      letterSet[letter] = true;
+    });
+    const uniqueLetters = Object.keys(letterSet);
+    if(uniqueLetters.length!==26){
+      return false;
+    };
+
     if(encode) {
       return translate(input,originalAlphabet,encodedAlphabet);
-    }
+    };
     return translate(input,encodedAlphabet,originalAlphabet);
   };
 
